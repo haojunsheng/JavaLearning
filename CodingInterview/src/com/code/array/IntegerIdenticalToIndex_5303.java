e18dc2d19b689d6632c2186693eb13b1ffd55d91
Anapodoton <anapodoton@qq.com>
2019-04-22 19:45:45 +0800

    add:53&66

---
+++ CodingInterview/src/com/code/array/IntegerIdenticalToIndex_5303.java
@@ -0,0 +1,4 @@
+package com.code.array;
+
+public class IntegerIdenticalToIndex_5303 {
+}
--- CodingInterview/src/com/code/array/NumbersAppearOnce_5602.java
+++ CodingInterview/src/com/code/array/NumbersAppearOnce_5602.java
@@ -1,32 +1,45 @@
 package com.code.array;
-//num1,num2分别为长度为1的数组。传出参数
-//将num1[0],num2[0]设置为返回结果
+
+/**
+ * 题目：
+ * 数组A中，除了某一个数字x之外，其他数字都出现了三次，而x出现了一次。请给出最快的方法找到x。
+ *
+ *如果数组中没有x，那么数组中所有的数字都出现了3次，在二进制上，每位上1的个数肯定也能被3整除。如{1, 5, 1, 5, 1, 5}从二进制上看有：
+ *
+ * 1：0001
+ *
+ * 5：0101
+ *
+ * 1：0001
+ *
+ * 5：0101
+ *
+ * 1：0001
+ *
+ * 5：0101
+ *
+ * 二进制第0位上有6个1，第2位上有3个1.第1位和第3位上都是0个1，每一位上的统计结果都可以被3整除。而再对该数组添加任何一个数，如果这个数在二进制的某位上为1都将导致该位上1的个数不能被3整除。因此通过统计二进制上每位1的个数就可以推断出x在该位置上是0还是1了，这样就能计算出x了。
+ */
 public class NumbersAppearOnce_5602 {
 
-    public void FindNumsAppearOnce(int [] array,int num1[] , int num2[]) {
-        num1[0]=0;
-        num2[0]=0;
-        if(array==null || array.length<=1){
-            return;
-        }
-        int result=array[0];
-        //将所有数进异或
-        for(int i=1;i<array.length;++i){
-            result ^= array[i];
-        }
-        int index=0;//找到result第一个为1的位置(从低位到高位)
-        for(index=0;index<32;++index){
-            if((result & (1<<index))!=0){
-                break;
+    public int FindNumsAppearOnce(int [] array) {
+        int length=array.length;
+        if(array==null || length%3!=1)
+            return 0;
+        //存储每位的和
+        int bits[]=new int[32];
+        for(int i=0;i<length;++i){
+            for(int j=0;j<32;j++){
+                bits[j]+=((array[i]>>>j)&1);
             }
         }
-        //把数组分为两个子数组，标准：数组中的元素的index位为1或者0
-        for(int i=0;i<array.length;++i){
-            if((array[i]&(1<<index))!=0){
-                num1[0]^=array[i];
-            }else {
-                num2[0]^=array[i];
+        //计算在该位上，如果该位对3取余等于0，那么唯一的那个数在该位为0，否则为1
+        int res = 0;
+        for(int i = 0; i < 32; i++){
+            if(bits[i] % 3 !=0){
+                res = res | (1 << i);
             }
         }
+        return res;
     }
 }
