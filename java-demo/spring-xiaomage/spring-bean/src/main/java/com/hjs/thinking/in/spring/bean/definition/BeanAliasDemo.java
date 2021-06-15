df5eb32d35f1eba4e2b315e26f408275959da643
haojunsheng <hjs.junyu@gmail.com>
2021-06-15 21:30:11 +0800

    add: spring

---
+++ java-demo/spring-xiaomage/spring-bean/src/main/java/com/hjs/thinking/in/spring/bean/definition/BeanAliasDemo.java
@@ -0,0 +1,22 @@
+package com.hjs.thinking.in.spring.bean.definition;
+
+import com.hjs.thinking.in.spring.ioc.overview.domain.User;
+import org.springframework.beans.factory.BeanFactory;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
+/**
+ * Bean 别名示例
+ * * @since
+ */
+public class BeanAliasDemo {
+
+    public static void main(String[] args) {
+        // 配置 XML 配置文件
+        // 启动 Spring 应用上下文
+        BeanFactory beanFactory = new ClassPathXmlApplicationContext("classpath:/META-INF/bean-definitions-context.xml");
+        // 通过别名 hjs-user 获取曾用名 user 的 bean
+        User user = beanFactory.getBean("user", User.class);
+        User xiaomageUser = beanFactory.getBean("hjs-user", User.class);
+        System.out.println(user == xiaomageUser);
+    }
+}
