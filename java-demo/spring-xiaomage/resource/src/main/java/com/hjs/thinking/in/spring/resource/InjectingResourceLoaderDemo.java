45cdadbdcb7fd699f78a4ec329deb0b9504a1452
haojunsheng <1324722673@qq.com>
2021-07-13 00:05:11 +0800

    add:spring doc

---
+++ java-demo/spring-xiaomage/resource/src/main/java/com/hjs/thinking/in/spring/resource/InjectingResourceLoaderDemo.java
@@ -0,0 +1,45 @@
+package com.hjs.thinking.in.spring.resource;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.ResourceLoaderAware;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+import org.springframework.context.support.AbstractApplicationContext;
+import org.springframework.core.io.ResourceLoader;
+
+import javax.annotation.PostConstruct;
+
+/**
+ * @author 俊语
+ * @date 2021/7/12 下午11:09
+ */
+public class InjectingResourceLoaderDemo implements ResourceLoaderAware {
+
+    private ResourceLoader resourceLoader; // 方法一
+
+    @Autowired
+    private ResourceLoader autowiredResourceLoader; // 方法二
+
+    @Autowired
+    private AbstractApplicationContext applicationContext; // 方法三
+
+    @PostConstruct
+    public void init() {
+        System.out.println("resourceLoader == autowiredResourceLoader : " + (resourceLoader == autowiredResourceLoader));
+        System.out.println("resourceLoader == applicationContext : " + (resourceLoader == applicationContext));
+    }
+
+    @Override
+    public void setResourceLoader(ResourceLoader resourceLoader) {
+        this.resourceLoader = resourceLoader;
+    }
+
+    public static void main(String[] args) {
+        AnnotationConfigApplicationContext context = new AnnotationConfigApplicationContext();
+        // 注册当前类作为 Configuration Class
+        context.register(InjectingResourceLoaderDemo.class);
+        // 启动 Spring 应用上下文
+        context.refresh();
+        // 关闭 Spring 应用上下文
+        context.close();
+    }
+}
