5e3a067f85dac0b691dc32c014e51eb6b07012f8
haojunsheng <1324722673@qq.com>
2021-07-14 23:59:42 +0800

    add: spring doc

--- java-demo/spring-xiaomage/ioc-container-overview/src/main/java/com/hjs/thinking/in/spring/ioc/overview/dependency/injection/DependencyInjectionDemo.java
+++ java-demo/spring-xiaomage/ioc-container-overview/src/main/java/com/hjs/thinking/in/spring/ioc/overview/dependency/injection/DependencyInjectionDemo.java
@@ -1,6 +1,7 @@
 package com.hjs.thinking.in.spring.ioc.overview.dependency.injection;
 
 import com.hjs.thinking.in.spring.ioc.overview.repository.UserRepository;
+import org.springframework.beans.factory.BeanFactory;
 import org.springframework.beans.factory.ObjectFactory;
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.support.ClassPathXmlApplicationContext;
@@ -8,6 +9,7 @@ import org.springframework.core.env.Environment;
 
 /**
  * 依赖注入示例
+ *
  * @author 俊语
  * @date 2021/6/8 下午10:50
  */
@@ -16,25 +18,26 @@ public class DependencyInjectionDemo {
     public static void main(String[] args) {
         // 配置 XML 配置文件
         // 启动 Spring 应用上下文
-//        BeanFactory beanFactory = new ClassPathXmlApplicationContext("classpath:/META-INF/dependency-injection-context.xml");
+        BeanFactory beanFactory = new ClassPathXmlApplicationContext("classpath:/META-INF/dependency-injection-context.xml");
 
         ApplicationContext applicationContext = new ClassPathXmlApplicationContext("classpath:/META-INF/dependency-injection-context.xml");
 
         // 依赖来源一：自定义 Bean
         UserRepository userRepository = applicationContext.getBean("userRepository", UserRepository.class);
 
-//        System.out.println(userRepository.getUsers());
+        System.out.println("自定义Bean: "+userRepository.getUsers());
 
         // 依赖来源二：依赖注入（內建依赖）,
-        System.out.println(userRepository.getBeanFactory());
+        System.out.println("内建依赖: "+userRepository.getBeanFactory());
+        System.out.println("内建Bean和自定义Bean: " + (userRepository.getObjectFactory() == beanFactory));
+        // 依赖查找（错误）
+//        System.out.println(beanFactory.getBean(BeanFactory.class));
 
 
         ObjectFactory userFactory = userRepository.getObjectFactory();
+        System.out.println(userFactory.getObject());
+        System.out.println("内建Bean" + (userFactory.getObject() == applicationContext));
 
-        System.out.println(userFactory.getObject() == applicationContext);
-
-        // 依赖查找（错误）
-//        System.out.println(beanFactory.getBean(BeanFactory.class));
 
         // 依赖来源三：容器內建 Bean
         Environment environment = applicationContext.getBean(Environment.class);
--- java-demo/spring-xiaomage/ioc-container-overview/src/main/java/com/hjs/thinking/in/spring/ioc/overview/dependency/lookup/DependencyLookupDemo.java
+++ java-demo/spring-xiaomage/ioc-container-overview/src/main/java/com/hjs/thinking/in/spring/ioc/overview/dependency/lookup/DependencyLookupDemo.java
@@ -12,6 +12,7 @@ import java.util.Map;
 /**
  * 依赖查找示例
  * 1. 通过名称的方式来查找
+ *
  * @author 俊语
  * @date 2021/6/8 下午10:32
  */
@@ -20,38 +21,56 @@ public class DependencyLookupDemo {
         // 配置 XML 配置文件
         // 启动 Spring 应用上下文
         BeanFactory beanFactory = new ClassPathXmlApplicationContext("classpath:/META-INF/dependency-lookup-context.xml");
+        // 按照名称查找
+        lookupByName(beanFactory);
         // 按照类型查找
         lookupByType(beanFactory);
-        // 按照类型查找结合对象
+        // 按照类型查找集合对象
         lookupCollectionByType(beanFactory);
         // 通过注解查找对象
         lookupByAnnotationType(beanFactory);
-
-//        lookupInRealTime(beanFactory);
-//        lookupInLazy(beanFactory);
     }
 
+    private static void lookupByName(BeanFactory beanFactory) {
+        System.out.println("####名称查找开始####");
+        lookupInRealTime(beanFactory);
+        lookupInLazy(beanFactory);
+        System.out.println("####名称查找结束####");
+    }
+
+
     private static void lookupByAnnotationType(BeanFactory beanFactory) {
+        System.out.println("####注解查找开始####");
         if (beanFactory instanceof ListableBeanFactory) {
             ListableBeanFactory listableBeanFactory = (ListableBeanFactory) beanFactory;
             Map<String, User> users = (Map) listableBeanFactory.getBeansWithAnnotation(Super.class);
             System.out.println("查找标注 @Super 所有的 User 集合对象：" + users);
         }
+        System.out.println("####注解查找结束####");
     }
 
     private static void lookupCollectionByType(BeanFactory beanFactory) {
+        System.out.println("####集合查找开始####");
         if (beanFactory instanceof ListableBeanFactory) {
             ListableBeanFactory listableBeanFactory = (ListableBeanFactory) beanFactory;
             Map<String, User> users = listableBeanFactory.getBeansOfType(User.class);
             System.out.println("查找到的所有的 User 集合对象：" + users);
         }
+        System.out.println("####集合查找结束####");
     }
 
     private static void lookupByType(BeanFactory beanFactory) {
+        System.out.println("####类型查找开始####");
         User user = beanFactory.getBean(User.class);
         System.out.println("实时查找：" + user);
+        System.out.println("####类型查找结束####");
     }
 
+    /**
+     * 延迟查找
+     *
+     * @param beanFactory
+     */
     private static void lookupInLazy(BeanFactory beanFactory) {
         ObjectFactory<User> objectFactory = (ObjectFactory<User>) beanFactory.getBean("objectFactory");
         User user = objectFactory.getObject();
